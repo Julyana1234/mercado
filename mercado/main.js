375a214345b4dab924a12e691c3eca77f314b1ef
Julyana1234 <61100622+Julyana1234@users.noreply.github.com>
2022-12-15 16:16:13 -0300

    Add files via upload

---
+++ mercado/main.js
@@ -0,0 +1,21 @@
+var nome = document.querySelector(".nome");
+
+function redirecionarParaLogin(){
+    window.location = "indexLogin.html";
+}
+
+function redirecionarParaHome(){
+    window.location = "indexHome.html";
+}
+
+function redirecionarParaBebidas(){
+    window.location = "indexBebidas.html";
+}
+
+function redirecionarParaAlimento(){
+    window.location = "indexAlimento.html";
+}
+
+function redirecionarParaLimpeza(){
+    window.location = "indexLimpeza.html";
+}
